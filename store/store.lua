6cdbe04578e4e1c76fe31a331f9367db32908aa8
NakeleKantoo <zzleozz2017@gmail.com>
2023-11-05 22:06:51 -0300

    Gambiarra mais bem feita tá?

--- store/store.lua
+++ store/store.lua
@@ -2,14 +2,14 @@
 function loadStoreItems()
     local obj={
         {name="Darkue",bought=false,price=500,font="fonts/Bricolage.ttf",color={0.05,0.05,0.05},textcolor={0.13,0.91,1.00},suitcolor={0.13,0.91,1.00},casered={0.64,0.17,1.00}},
-        {name="Darkue",bought=false,price=500,font="fonts/Bricolage.ttf",color={0.05,0.05,0.05},textcolor={0.13,0.91,1.00},suitcolor={0.13,0.91,1.00},casered={0.64,0.17,1.00}},
-        {name="Darkue",bought=false,price=500,font="fonts/Bricolage.ttf",color={0.05,0.05,0.05},textcolor={0.13,0.91,1.00},suitcolor={0.13,0.91,1.00},casered={0.64,0.17,1.00}},
-        {name="Darkue",bought=false,price=500,font="fonts/Bricolage.ttf",color={0.05,0.05,0.05},textcolor={0.13,0.91,1.00},suitcolor={0.13,0.91,1.00},casered={0.64,0.17,1.00}},
-        {name="Darkue",bought=false,price=500,font="fonts/Bricolage.ttf",color={0.05,0.05,0.05},textcolor={0.13,0.91,1.00},suitcolor={0.13,0.91,1.00},casered={0.64,0.17,1.00}},
-        {name="Darkue",bought=false,price=500,font="fonts/Bricolage.ttf",color={0.05,0.05,0.05},textcolor={0.13,0.91,1.00},suitcolor={0.13,0.91,1.00},casered={0.64,0.17,1.00}},
-        {name="Darkue",bought=false,price=500,font="fonts/Bricolage.ttf",color={0.05,0.05,0.05},textcolor={0.13,0.91,1.00},suitcolor={0.13,0.91,1.00},casered={0.64,0.17,1.00}},
-        {name="Darkue",bought=false,price=500,font="fonts/Bricolage.ttf",color={0.05,0.05,0.05},textcolor={0.13,0.91,1.00},suitcolor={0.13,0.91,1.00},casered={0.64,0.17,1.00}},
-        {name="Darkue",bought=false,price=500,font="fonts/Bricolage.ttf",color={0.05,0.05,0.05},textcolor={0.13,0.91,1.00},suitcolor={0.13,0.91,1.00},casered={0.64,0.17,1.00}},
+        --{name="Darkue",bought=false,price=500,font="fonts/Bricolage.ttf",color={0.05,0.05,0.05},textcolor={0.13,0.91,1.00},suitcolor={0.13,0.91,1.00},casered={0.64,0.17,1.00}},
+        --{name="Darkue",bought=false,price=500,font="fonts/Bricolage.ttf",color={0.05,0.05,0.05},textcolor={0.13,0.91,1.00},suitcolor={0.13,0.91,1.00},casered={0.64,0.17,1.00}},
+        --{name="Darkue",bought=false,price=500,font="fonts/Bricolage.ttf",color={0.05,0.05,0.05},textcolor={0.13,0.91,1.00},suitcolor={0.13,0.91,1.00},casered={0.64,0.17,1.00}},
+        --{name="Darkue",bought=false,price=500,font="fonts/Bricolage.ttf",color={0.05,0.05,0.05},textcolor={0.13,0.91,1.00},suitcolor={0.13,0.91,1.00},casered={0.64,0.17,1.00}},
+        --{name="Darkue",bought=false,price=500,font="fonts/Bricolage.ttf",color={0.05,0.05,0.05},textcolor={0.13,0.91,1.00},suitcolor={0.13,0.91,1.00},casered={0.64,0.17,1.00}},
+        --{name="Darkue",bought=false,price=500,font="fonts/Bricolage.ttf",color={0.05,0.05,0.05},textcolor={0.13,0.91,1.00},suitcolor={0.13,0.91,1.00},casered={0.64,0.17,1.00}},
+        --{name="Darkue",bought=false,price=500,font="fonts/Bricolage.ttf",color={0.05,0.05,0.05},textcolor={0.13,0.91,1.00},suitcolor={0.13,0.91,1.00},casered={0.64,0.17,1.00}},
+        --{name="Darkue",bought=false,price=500,font="fonts/Bricolage.ttf",color={0.05,0.05,0.05},textcolor={0.13,0.91,1.00},suitcolor={0.13,0.91,1.00},casered={0.64,0.17,1.00}},
         {name="Darked",bought=false,price=500,font="fonts/Bricolage.ttf",color={0.05,0.05,0.05},textcolor={0.01,1.00,0.24},suitcolor={0.01,1.00,0.24},casered={1.00,0.03,0.22}}
     }
     return obj
